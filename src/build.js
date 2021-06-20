dd05f1603abce2c74a21c7f772019883acb7e543
Olli Paloviita <olli.paloviita@gmail.com>
2021-06-20 21:33:52 +0300

    build.js refactored, body.js renamed to main.js
    Now mockPage.html changes are taken into use automatically

--- src/build.js
+++ src/build.js
@@ -1,26 +1,33 @@
 /*
 Run this file to generate new classified.html file
 */
-
-const { header, footer, getIntro } = require('./misc');
 const path = require('path');
 const fs = require('fs');
 
-// read files
-const body = fs.readFileSync(path.join(__dirname, 'body.js'), 'utf8');
-const base64js = fs.readFileSync(path.join(__dirname, './base64-js.js'), 'utf8');
-const license = fs.readFileSync(path.join(__dirname, '..', './LICENSE'), 'utf8');
+// read the required files
+const read = (file) => fs.readFileSync(path.join(__dirname, file), 'utf8');
+let main = read('main.js');
+const base64js = read('./base64-js.js');
+const license = read('../LICENSE');
+const mockPage = read('mockPage.html');
 
-// get file, filepath
+// replace main.js keywords with other files
+const mainReplace = (name, file) => (main = main.replace(name, file.trim()));
+mainReplace('LICENCE', license);
+mainReplace('// BASE64-JS', base64js);
+mainReplace('SVG-TERMS', mockPage.split('SVG-TERMS')[1]);
+mainReplace('/*CSS*/', mockPage.split('/*CSS*/')[1]);
+mainReplace('<!--HTML-->', mockPage.split('<!--HTML-->')[1]);
+
+// build the final file
 const fileContents = (
-`${header}
-${getIntro(license)}
-${base64js}
-${body}
-${footer}`);
-const filePath = path.join(__dirname, '..', 'classified.html');
+`//<!DOCTYPE html><html lang='en'><head><meta charset='utf-8'><meta name='viewport' content='width=device-width'></head><body><script>
+${main}
+//</script></body></html>`);
 
 // write file
+const filePath = path.join(__dirname, '..', 'classified.html');
 fs.writeFileSync(filePath, fileContents, 'utf8');
 
+// log finish status
 console.log(`Build finished: ${filePath}`);
