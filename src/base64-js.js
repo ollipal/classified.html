9323bc6953e415a4ccd00baf30b2eec5000509f5
Olli Paloviita <olli.paloviita@gmail.com>
2021-08-09 17:08:28 +0300

    browser: alert in addition of logging errors that happens in browser

--- src/base64-js.js
+++ src/base64-js.js
@@ -1,8 +1,8 @@
-/*
- * Below is base64-js's index.js from: https://github.com/beatgammit/base64-js/blob/master/index.js
- * This library allows consistent base64 encoding between browsers and Node.js
- * 2021-02-20, commit 88957c9943c7e2a0f03cdf73e71d579e433627d3
- * Modifications: this comment, and the comment at the end, exports have been commented out
+  /*
+  * Below is base64-js's index.js from: https://github.com/beatgammit/base64-js/blob/master/index.js
+  * This library allows consistent base64 encoding between browsers and Node.js
+  * 2021-02-20, commit 88957c9943c7e2a0f03cdf73e71d579e433627d3
+  * Modifications: this comment, and the comment at the end, exports have been commented out
 
 The MIT License (MIT)
 
@@ -178,6 +178,6 @@ function fromByteArray (uint8) {
   return parts.join('')
 }
 
-/*
- * base64-js end
- */
+  /*
+  * base64-js end
+  */
